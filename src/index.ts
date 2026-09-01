b7f4a6fa5af91401d9f2cc2f7ffd74e71844f5a2
owner-owner <abdeallahe72@gmail.com>
2026-09-01 20:18:30 +0200

    index.ts

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ process.on('uncaughtException', (err: Error) => {
 const BOT_CONFIG = {
   host: 'zero7even.net',
   port: 25565,
-  username: 'LO07JDY0',
+  username: 'atiolp_spawner',
   version: '1.20.4',
 };
 
