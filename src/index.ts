5bff5b8d070f52db588d63435596870ca4e8fda3
owner-owner <abdeallahe72@gmail.com>
2026-09-18 21:07:32 +0200

    index.ts

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ process.on('uncaughtException', (err: Error) => {
 const BOT_CONFIG = {
   host: 'zero7even.net',
   port: 25565,
-  username: 'LZADGREE',
+  username: 'LZADGRE',
   version: '1.20.4',
 };
 
